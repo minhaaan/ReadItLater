751f3a47c3011eab426bb028d73f7bd133659cfc
minhaaan <chmh0411@gmail.com>
2025-03-27 01:11:56 +0900

    InputView 함수 분리

--- ReadItLater/Sources/Main/MainView.swift
+++ ReadItLater/Sources/Main/MainView.swift
@@ -47,42 +47,47 @@ struct MainView: View {
   var body: some View {
     WithPerceptionTracking {
       VStack {
-        HStack {
-          TextField(
-            "입력",
-            text: $store.inputFieldText.sending(\.textFieldChanged)
-          )
-          .padding(20)
-          .background(
-            RoundedRectangle(cornerRadius: 12)
-              .stroke(.black.opacity(0.4), lineWidth: 2)
-          )
-          .submitLabel(.done)
-          .onSubmit {
-            store.send(.save)
-          }
-          
-          if !store.writeButtonIsHidden {
-            Button(action: {
-              store.send(.save)
-            }) {
-              Image(systemName: "checkmark.circle.fill")
-                .font(.title)
-                .padding(12)
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-            }
-            .padding(.leading, 8)
-            .transition(.move(edge: .trailing).combined(with: .opacity))
-            .animation(.easeInOut(duration: 0.3), value: store.writeButtonIsHidden)
-          }
-        } // HStack
-        .padding(.horizontal, 20)
+        inputView()
         
         Spacer()
       } // VStack
     }
+  }
+  
+  /// 유저 입력을 받는 뷰
+  private func inputView() -> some View {
+    HStack {
+      TextField(
+        "입력",
+        text: $store.inputFieldText.sending(\.textFieldChanged)
+      )
+      .padding(20)
+      .background(
+        RoundedRectangle(cornerRadius: 12)
+          .stroke(.black.opacity(0.4), lineWidth: 2)
+      )
+      .submitLabel(.done)
+      .onSubmit {
+        store.send(.save)
+      }
+      
+      if !store.writeButtonIsHidden {
+        Button(action: {
+          store.send(.save)
+        }) {
+          Image(systemName: "checkmark.circle.fill")
+            .font(.title)
+            .padding(12)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+        }
+        .padding(.leading, 8)
+        .transition(.move(edge: .trailing).combined(with: .opacity))
+        .animation(.easeInOut(duration: 0.3), value: store.writeButtonIsHidden)
+      }
+    } // HStack
+    .padding(.horizontal, 20)
   }
   
 }
