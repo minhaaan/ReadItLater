c232f2d191b3e05a6f68d02b125c49485a7634e8
minhaaan <chmh0411@gmail.com>
2025-03-28 04:25:20 +0900

    Reducer 분리

---
+++ Projects/ReadItLater/Sources/Main/Subviews/MainList.swift
@@ -0,0 +1,66 @@
+import Foundation
+import ComposableArchitecture
+import Perception
+import ReadItLaterStorage
+
+@Reducer
+struct MainList {
+  
+  @ObservableState
+  struct State: Equatable {
+    var items: [SharedItem] = []
+  }
+  
+  enum Action {
+    case onAppear
+    case loaded([SharedItem])
+    case insert(String)
+    case delete(IndexSet)
+  }
+  
+  @Dependency(\.readItLaterStorage) var readItLaterStorage
+  
+  var body: some Reducer<State, Action> {
+    Reduce { state, action in
+      switch action {
+      case .onAppear:
+        // 저장된 목록 로드
+        return .run { send in
+          do {
+            let sharedItems = try await readItLaterStorage.loadAll()
+            await send(.loaded(sharedItems))
+          } catch {
+            // 에러 처리
+          }
+        }
+        
+      case let .loaded(items):
+        state.items = items
+        return .none
+        
+      case let .insert(text):
+        let item = SharedItem(text: text, date: Date())
+        return .run { send in
+          do {
+            try await readItLaterStorage.save(item)
+            let updatedItems = try await readItLaterStorage.loadAll()
+            await send(.loaded(updatedItems))
+          } catch {
+            // 필요하면 에러 처리
+          }
+        }
+        
+      case let .delete(indexSet):
+        return .run { send in
+          do {
+            try await readItLaterStorage.removeAt(indexSet)
+            let updatedItems = try await readItLaterStorage.loadAll()
+            await send(.loaded(updatedItems))
+          } catch {
+            // 필요하면 에러 처리
+          }
+        }
+      }
+    }
+  }
+}
